b559012ecc57b9bd5f41fe61d4583f675b4d555c
xivSolutions <xivSolutions@gmail.com>
2013-07-01 18:53:45 -0500

    Initial Commit - Mostly empty WebApiProject

---
+++ EmptyWebApiTemplateProject/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace EmptyWebApiTemplateProject
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+        }
+    }
+}
